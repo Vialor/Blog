b68f75e5fe427f7ac156fbcd51d103cd17f16721
Vialor <42876369+Vialor@users.noreply.github.com>
2020-05-11 13:32:59 -0400

    fun page

--- blog/pages/fun.js
+++ blog/pages/fun.js
@@ -2,6 +2,40 @@ import Layout from '../components/layout.js'
 
 export default function Home() {
   return (
-    <Layout/>
-  )
+    <Layout>
+      <h1>JS Cool Stuff</h1>
+      <div className="js-cool-stuff fun-project">
+        <img className="project-image" src="/static/img/fun/js.png" href="http://github.com/Vialor/JS-cool-stuff" alt="js"/>
+        <div className="project-intro">
+          <p> A github repository to try out some cool JavaScript and CSS (mainly) ideas, including a
+            simple piano, 2048 game, timer and more.</p>
+          <p> To see my work, use your browser to open the files, "index.html".</p>
+        </div>
+      </div>
+      <h1>Planes</h1>
+      <div className="planes fun-project">
+        <img className="project-image" src="/static/img/fun/planes.png" href="https://github.com/Vialor/Planes" alt="planes"/>
+        <div className="project-intro">
+          <p> A simple plane game made with pygame.</p>
+          <p> You need both python3 and pygame to run this game.</p>
+        </div>
+      </div>
+  <style jsx>{`.fun-project{
+      display:flex;
+      align-items: center;
+  }
+
+  .fun-project .project-image{
+      width: 100px;
+  }
+
+  .fun-project a{
+      flex-basis: 25%;
+  }
+
+  .fun-project .project-intro{
+      flex-basis: 75%;
+  }`}</style>
+    </Layout>
+  );
 }
--- blog/pages/math.js
+++ blog/pages/math.js
@@ -2,6 +2,18 @@ import Layout from '../components/layout.js'
 
 export default function Home() {
   return (
-    <Layout/>
-  )
+    <Layout>
+      <iframe src="/static/pdf/Calculus.pdf" id="my-frame" height="100%" width="100%">
+        Your Browser Does Not Support PDF, Please Download This File: 
+      <a href="/static/pdf/LinearAlgebra.pdf" id="math-pdf-download">Download PDF</a></iframe>
+      <style jsx>{`
+        iframe{
+          height: 90vh;
+          border: solid lightblue 3px;
+          border-radius: 5px;
+          box-shadow: 0 0 5px lightslategray;
+        }`
+      }</style>
+    </Layout>
+  );
 }
